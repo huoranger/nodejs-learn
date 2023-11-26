70df8d185283162aafeceb58b48b591ea739a924
huoranger <huoranger@163.com>
2023-11-26 15:32:06 +0800

    project init

---
+++ 01_hello_js/index.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+    console.log("Hello JavaScript~");
+}, 1000);
+
+console.log(process);
---
+++ 01_hello_js/如何使用n.md
@@ -0,0 +1,10 @@
+使用 npm 安装 n
+```shell
+npm install n -g
+```
+
+安装 node 版本
+```shell
+# 最新长期支持版本
+n lts
+```
